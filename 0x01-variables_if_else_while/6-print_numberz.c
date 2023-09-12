a2cf5a97688fadc7b515293e19d2d627f6906354
Jeegz <josiahjonathan24@gmail.com>
2023-09-11 21:09:00 -0700

    print 6 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m;
+
+	m = 0;
+	while
+		(m < 10) {
+			putchar(m + '0');
+			m++;
+		}
+	putchar('\n');
+	return (0);
+}
